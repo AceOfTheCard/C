05b493dbd28bb75ff61387b52eac812d5fb9cdf8
AceOfTheCard <117891298+AceOfTheCard@users.noreply.github.com>
2025-07-25 13:24:49 +0200

    Studen Structure

--- student.c
+++ student.c
@@ -3,8 +3,7 @@
 #include <string.h>
 
 
-// A dynamic memory collection 
-
+#pragma pack(1)
 typedef struct Student{
     char* name; // dynamic size rather than fixed size
     int age;
@@ -14,6 +13,8 @@ typedef struct Student{
 
 Student* addStudent(int age, char* name, int id){
     Student* student = (Student*)malloc(sizeof(Student));
+    printf("The based address of the student block is %p\n",&student);
+    printf("The ending address of the student block is %p\n",((char*)&student +sizeof(student)));
     if(!student) return NULL;  
     student->age = age;
     student->id = id;
@@ -25,13 +26,14 @@ Student* addStudent(int age, char* name, int id){
     }
     strcpy(student->name,name);
 
-    printf("A new student is created");
+    printf("A new student is created \n");
     return student;
 }
 
 
 int main(){
     Student* student =  addStudent(13,"Anna",1);
+    printf("The size of a char pointer is %d ",sizeof(Student));
     return 0;
 }
 
