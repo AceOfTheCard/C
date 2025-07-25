dbbad8525db13897078ac6021bc8e9efc31c5f7b
AceOfTheCard <117891298+AceOfTheCard@users.noreply.github.com>
2025-07-25 12:19:51 +0200

    A new student

--- student.c
+++ student.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
+// A dynamic memory collection 
 
-int SquareSum(){};
+typedef struct Student{
+    char* name; // dynamic size rather than fixed size
+    int age;
+    int id;
+}Student;
+
+
+Student* addStudent(int age, char* name, int id){
+    Student* student = (Student*)malloc(sizeof(Student));
+    if(!student) return NULL;  
+    student->age = age;
+    student->id = id;
+     
+    student->name = (char*)malloc(sizeof(strlen(name)+1)); 
+    if(!student->name){
+        free(student);
+        return NULL;
+    }
+    strcpy(student->name,name);
+
+    printf("A new student is created");
+    return student;
+}
+
+
 int main(){
-    printf("This function is not something that we would like to work on");
-} 
+    Student* student =  addStudent(13,"Anna",1);
+    return 0;
+}
+
 
 
