f04e00ae121b2ea56883f9a87a4506427c5b3158
AceOfTheCard <117891298+AceOfTheCard@users.noreply.github.com>
2025-07-27 17:59:32 +0200

    Flokshi CV

---
+++ cv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+void printHelloMessage(){
+    printf("Hello, Dear.\n");
+    printf("So happy you are here\n");
+    printf("Welcome to my CV representation\n");
+    printf("This is not like the other CV, because I am not like the others.\n");
+}
+
+
+int main(){
+    printHelloMessage();
+    return 0;
+
+}
